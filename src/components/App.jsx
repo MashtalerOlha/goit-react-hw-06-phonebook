51a374e07619863c11709a512f317250ca768549
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-05-16 22:22:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,17 @@ import { Form } from './Form/Form';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { Container } from './Container.Styled';
-import { useContactChange } from './redux/contactsSlice';
 import { Toaster } from 'react-hot-toast';
 
-
 export function App() {
-  const {filteredItems, deleteContact,addNewContact, changeFilter, getVisibleContacts} = useContactChange()
-
   return (
     <Container>
       <h1>Phonebook</h1>
-      <Form onSubmit={addNewContact} />
+      <Form />
       <h2>Contacts</h2>
-      <Filter value={filteredItems} onChange={changeFilter} />
-      <ContactList
-        onDeleteContact={deleteContact}
-        items={getVisibleContacts()}
-      />
-      <Toaster/>
+      <Filter />
+      <ContactList />
+      <Toaster />
     </Container>
   );
 }
